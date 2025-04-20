1516335b416406fbd705c79122969e9afe207deb
Lakshman <ayodhya.lakshman1111@gmail.com>
2025-04-20 13:05:34 +0530

    added the DB connection to project

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("Lakshman Ayodhya");
+
+const express = require("express")
+const pool = require("./config/dbConnection")
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`);
+})
